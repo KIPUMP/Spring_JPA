d7a3778e697ab785be2763641a3961e122c1752c
KIPUMP <kibeom2000@naver.com>
2024-07-22 15:43:03 +0900

    add memo files

--- memo/src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ memo/src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -3,10 +3,10 @@ package com.sparta.memo.repository;
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.*;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
     List<Memo> findAllByOrderByModifiedAtDesc();
-    List<Memo> findAllByUsername(String username);
+    List<Memo> findAllByContentsContainsOrderByModifiedAtDesc(String name);
 
 }
--- memo/src/main/java/com/sparta/memo/service/MemoService.java
+++ memo/src/main/java/com/sparta/memo/service/MemoService.java
@@ -42,7 +42,6 @@ public class MemoService {
         memo.update(requestDto);
         return id;
     }
-
     public Long deleteMemo(Long id) {
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = findMemo(id);
@@ -50,9 +49,12 @@ public class MemoService {
         memoRepository.delete(memo);
         return id;
     }
+    public List<MemoResponseDto> getMemosByKeyword(String keyword){
+        return memoRepository.findAllByContentsContainsOrderByModifiedAtDesc(keyword).stream().map(MemoResponseDto::new).toList();
+    }
 
     private Memo findMemo(Long id) {
-        return  memoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("선택한 메뉴는 없습니다"));
+        return  memoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("선택한 메모는 존재하지 않습니다"));
     }
 
 }
