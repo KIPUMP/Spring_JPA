8c5b3e27534f4817bd9fc5ccbce0e5d3eaf5bb5f
KIPUMP <kibeom2000@naver.com>
2024-07-24 17:29:39 +0900

    add memo files

---
+++ spring-auth/src/main/java/com/sparta/spring_auth/food/Chicken.java
@@ -0,0 +1,15 @@
+package com.sparta.spring_auth.food;
+
+import jakarta.annotation.Priority;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class Chicken implements Food{
+
+    @Override
+    public void eat() {
+        System.out.println("치킨을 먹습니다");
+    }
+}
