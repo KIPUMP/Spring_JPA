1990b643fcf5142b511347a699ee10ac6ae1590d
KIPUMP <kibeom2000@naver.com>
2024-07-29 17:46:41 +0900

    add files validation files

--- spring-auth/src/main/java/com/sparta/spring_auth/controller/ProductController.java
+++ spring-auth/src/main/java/com/sparta/spring_auth/controller/ProductController.java
@@ -1,13 +1,21 @@
 package com.sparta.spring_auth.controller;
 
+import com.sparta.spring_auth.dto.ProductRequestDto;
 import com.sparta.spring_auth.entity.User;
+import com.sparta.spring_auth.entity.UserRoleEnum;
+import com.sparta.spring_auth.security.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api")
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class ProductController {
 
     @GetMapping("/products")
@@ -17,5 +25,22 @@ public class ProductController {
         System.out.println("user.getUsername() = " + user.getUsername());
 
         return "redirect:/";
+    }
+
+    @Secured(UserRoleEnum.Authority.ADMIN) // 관리자용
+    @GetMapping("/products/secured")
+    public String getProductsByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
+        for (GrantedAuthority authority : userDetails.getAuthorities()) {
+            System.out.println("authority.getAuthority() = " + authority.getAuthority());
+        }
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/validation")
+    @ResponseBody
+    public ProductRequestDto testValid(@RequestBody @Valid ProductRequestDto requestDto) {
+        return requestDto;
     }
 }
