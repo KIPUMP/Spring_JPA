e0bcfe9a17cf7eb47e76498d2d1c497fd350480c
KIPUMP <kibeom2000@naver.com>
2024-08-08 11:26:59 +0900

    add jpa_advanced files

---
+++ jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Order.java
@@ -0,0 +1,32 @@
+package com.sparta.jpaadvance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "food_id")
+    private Food food;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime orderDate;
+
+}
